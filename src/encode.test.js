bc590c5d4198295e54796ec9549da062af161141
goloto <mikhail@goloto.dev>
2024-04-19 13:53:38 +0300

    added todo encoding tests

---
+++ src/encode.test.js
@@ -0,0 +1,15 @@
+import { test, describe } from "bun:test";
+
+describe('Testing encoding *.dlstrings/*.ilstrings files', () => {
+  test.todo('Should encode without errors');
+  test.todo('Should correctly encode headers');
+  test.todo('Elements count should be equal to source elements count');
+  test.todo('Hash of decoded result should be equal to hash of source file');
+});
+  
+describe('Testing encoding *.strings files', () => {
+  test.todo('Should encode without errors');
+  test.todo('Should correctly encode headers');
+  test.todo('Elements count should be equal to source elements count');
+  test.todo('Hash of decoded result should be equal to hash of source file');
+});
